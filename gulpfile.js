abdaeae4adfcf95bb152dfba4064b466937ad045
schietop <schietop@hotmail.com>
2015-09-07 12:51:58 +0200

    Added static File Monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var jsSources = [
 	'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
@@ -39,25 +41,34 @@ gulp.task('compass', function() {
     })
     .on('error', gutil.log))
     .pipe(gulp.dest('builds/development/css'))
-    .pipe(connect.reload()) //dit toegevoegd anders deed hij geen reload
+    .pipe(connect.reload())
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch(htmlSources, ['html']);
+  gulp.watch(jsonSources, ['json']);
 })
 
-gulp.task("connect", function(){
+gulp.task('connect', function() {
   connect.server({
     root: 'builds/development/',
     livereload: true
   });
-})
+});
 
+gulp.task('html', function() {
+  gulp.src(htmlSources)
+    .pipe(connect.reload())
+});
 
+gulp.task('json', function() {
+  gulp.src(jsonSources)
+    .pipe(connect.reload())
+});
 
-
-gulp.task('default', ['coffee','js','compass', 'connect', 'watch']);
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
 
 
